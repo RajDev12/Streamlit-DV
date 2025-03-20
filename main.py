d3c52819df6553fb06b3f142176f7a4d56521441
RajDev12 <rjnath98@gmail.com>
2025-03-20 12:35:35 +0530

    uploader done

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+st.set_page_config(page_title="Data Visualization with Streamlit",
+                   page_icon="😁"
+                   
+)
+st.title("Data Visualization with Streamlit")
+with st.sidebar:
+    upload=st.file_uploader("Upload CSV")
+
+
+if upload is not None:
+    df=pd.read_csv(upload)
+    st.dataframe(df.head())
