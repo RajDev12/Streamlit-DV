d31b7ef6e4f6a3dcf59fbd23774b94b5d30d1b74
RajDev12 <rjnath98@gmail.com>
2025-03-21 12:08:23 +0530

    modified main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ st.set_page_config(page_title="Data Visualization with Streamlit",
                    page_icon="😁"
                    
 )
+st.write("HI this is new chnages")
 st.title("Data Visualization with Streamlit")
 with st.sidebar:
     upload=st.file_uploader("Upload CSV")
